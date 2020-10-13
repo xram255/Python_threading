ccdb4295c34c68272d36861cc6676089a4e8dab4
xram255 <roshan.cs790@gmail.com>
2020-10-13 15:08:40 +0530

    thread join

--- init.py
+++ init.py
@@ -9,11 +9,18 @@ def do_something():
     print(" done sleeping ")
 
 t1 = threading.Thread(target=do_something)
+time.sleep(.1)
 t2 = threading.Thread(target=do_something)
 
 t1.start()
 t2.start()
 
+t1.join()
+t2.join()
+
 stop = time.perf_counter()
 
 print(round(stop - start))
+
+for _ in range(5):
+    print("Hi")
